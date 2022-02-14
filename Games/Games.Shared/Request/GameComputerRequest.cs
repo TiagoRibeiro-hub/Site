b3394437161ac56d2569b219455046c0581c1cd8
Tiago Ribeiro <tiagovrib@gmail.com>
2022-02-14 14:05:06 +0000

    migration

--- Games/Games.Shared/Request/GameComputerRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Games.Shared;
-#nullable disable
-public class GameComputerRequest : Request
-{
-    public int IdGame { get; set; }
-    public string Difficulty { get; set; }
-    public List<int> PossibleMoves { get; set; }
-}
