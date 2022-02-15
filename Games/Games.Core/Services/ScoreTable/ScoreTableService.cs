1baf76ef66c0b2ef230dc645a0330d23f67efb38
Tiago Ribeiro <tiagovrib@gmail.com>
2022-02-15 10:54:31 +0000

    initialize scoretable

--- Games/Games.Core/Services/ScoreTable/ScoreTableService.cs
+++ Games/Games.Core/Services/ScoreTable/ScoreTableService.cs
@@ -30,34 +30,19 @@ internal class ScoreTableService : IScoreTableService
     public async Task TableScoreInitializeList(HashSet<ScoresTableEntity> scoresTableList)
     {
         int count = 0;
-        bool isRegistered_1 = false, isRegistered_2 = false;
+        bool isRegistered = false;
         foreach (var item in scoresTableList)
         {
             _expression = x => x.PlayerName == item.Email;
-            if (count == 0)
+            isRegistered = await _readRepository.IsAnyAsync(_expression);
+            if (isRegistered)
             {
-                count += 1;
-                isRegistered_1 = await _readRepository.IsAnyAsync(_expression);
+
             }
             else
             {
-                isRegistered_2 = await _readRepository.IsAnyAsync(_expression);
+
             }
-        }
-        if (isRegistered_1)
-        {
-
-        }
-        else
-        {
-
-        }
-        if (isRegistered_2)
-        {
-
-        }
-        else
-        {
 
         }
     }
